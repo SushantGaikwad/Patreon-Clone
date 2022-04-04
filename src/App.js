c7c0fb29627b6754b90cba4cb605878181a60cfb
Sushant Gaikwad <sushantgaikwad06@gmail.com>
2022-04-05 02:44:30 +0530

    Pro and Premium Added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Lite from './Components/Products/Lite';
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
-import Signup from './Components/Login-Signup/Signup';
+import {Routes,Route, Link} from "react-router-dom";
+import Pro from './Components/Products/Pro';
+import Premium from './Components/Products/Premium';
 
 
 function App() {
   return (
-    <div className="App">      
+    <div className="App">
+      <Link style={{color:"white",fontSize:"50px"}} to="/premium"> Premium </Link>     
+      <Link style={{color:"white",fontSize:"50px"}} to="/pro"> Pro </Link>     
+      <Link style={{color:"white",fontSize:"50px"}} to="/"> Lite </Link>     
           <Routes>
               <Route path='/' element={<Lite />} />
-              <Route path='/signup' element={<Signup />} />
+              <Route path='/premium' element={<Premium />} />
+              <Route path='/pro' element={<Pro />} />
           </Routes>
         {/* <div style={{height:"300px", width:"400px",background:  "black", position:"absolute",zIndex:"1"}}>Hello</div>
         <div style={{height:"300px", width:"200px",background:  "red",zIndex:"1",position:"absolute"}}>Sushant</div> */}
