f38d307b464587c3c55292f71d347428e88d5ac7
Sushant Subhash Gaikwad <sushantgaikwad06@gmail.com>
2022-04-06 18:01:00 +0530

    changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
 import React from 'react';
-import {Nav} from './Components/Nav-Blog/Nav'
-import {Login} from './Components/Login/Login'
-// import {Blog} from "./Components/Blogs/Blog"
-import {Mblog} from "./Components/MainBlog/Mblog"
-import { MblogNav } from './Components/MainBlog/MblogNav';
-// import {Patreon} from "./Components/MainBlog/Patreon"
-// import {Product } from './Components/MainBlog/Product';
-// import {PateronU } from './Components/MainBlog/PateronU';
-// import {Creator} from './Components/MainBlog/Creator';
-// import {Marketing} from './Components/MainBlog/Marketing'
-// import {Explore} from './Components/MainBlog/Explore'
-// import { AllPateronWork } from './Components/MainBlog/HQ/AllPateronWork';
-// import { Announcements } from './Components/MainBlog/HQ/Announcements';
-// import { HowWeWork } from './Components/MainBlog/HQ/HowWeWork';
-// import { PateronEvents } from './Components/MainBlog/HQ/PateronEvents';
-// import { BussinessOrganizations } from './Components/MainBlog/Explore/BussinessOrganizations';
-// import { CommunitiesEducation } from './Components/MainBlog/Explore/CommunitiesEducation';
-// import { Illustrators } from './Components/MainBlog/Explore/Illustrators';
-// import { LiveStreamers } from './Components/MainBlog/Explore/LiveStreamers';
-// import { ModelingCosplay } from './Components/MainBlog/Explore/ModelingCosplay';
-// import { Musicians } from './Components/MainBlog/Explore/Musicians';
-// import { Podcasters } from './Components/MainBlog/Explore/Podcasters';
-// import { Writers } from './Components/MainBlog/Explore/Writers';
-// import { Youtubers } from './Components/MainBlog/Explore/Youtubers';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import HomePage from './Components/HomePage/HomePage.jsx';
+import Footer from './Components/Footer/Footer';
+import Lite from "./Components/Products/Lite"
+import Pro from "./Components/Products/Pro"
+import Premium from "./Components/Products/Premium"
+import Podcasts from './Components/Creators/Podcasts';
 
-
-
- 
 function App() {
   return (
-   <>
-   {/* <React.Fragment>
-
-</React.Fragment> */}
-    <Nav />  
-    <MblogNav />
-    <Mblog />
-    {/* <Login /> */}
-  
-   </>
+    <div className="App">
+      <Router>
+       <Navbar />
+        {/* <HomePage/> */} 
+       
+        <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path='/lite' element={<Lite />} />
+        <Route path='/pro' element={<Pro />} />
+        <Route path='/premium' element={<Premium />} />
+        <Route path='/podcasters' element={<Podcasts />} />
+        
+        </Routes>
+        <Footer/>
+      </Router>
+      
+    </div>
   );
 }
 
